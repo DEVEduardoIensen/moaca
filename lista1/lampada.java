98964b4d7e9cd0a6d418c032895daef5ebc1cedd
DEVEduardoiensen <dev.eduardoiensen@gmail.com>
2026-03-13 20:20:02 -0300

    lista

---
+++ lista1/lampada.java
@@ -0,0 +1,33 @@
+package lista1;
+
+class Lampada {
+    boolean ligada;
+    int potencia = 40;
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+        potencia = 0;
+    }
+
+    public void meialuz() {
+        ligada = true;
+        potencia = 20;
+    }
+
+    public static void main(String[] args) {
+        Lampada top = new Lampada();
+
+        top.ligar();
+        System.out.println("Ligada: " + top.ligada + " potencia: " + top.potencia);
+
+        top.desligar();
+        System.out.println("Ligada:" + top.ligada + "potencia:" + top.potencia);
+
+        top.meialuz();
+        System.out.println("Ligada:" + top.ligada + "potencia:" + top.potencia);
+    }
+}
